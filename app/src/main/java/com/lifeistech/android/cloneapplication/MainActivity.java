a6190ddfbf50a9ce69404bd3ef7844cc76f5d106
uchikimi <uchikimi@gmail.com>
2016-02-15 20:11:51 +0900

    また変数追加

--- app/src/main/java/com/lifeistech/android/cloneapplication/MainActivity.java
+++ app/src/main/java/com/lifeistech/android/cloneapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     int mikit;
+    int mikit2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
